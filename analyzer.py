b0b9a6df9a9ec51cdef341db0782519e6d35a2aa
ahmed-seggane <323275509+ahmed-seggane@users.noreply.github.com>
2026-09-13 15:04:02 +0200

    Extract IP addresses from log lines

---
+++ analyzer.py
@@ -0,0 +1,21 @@
+lignes = [
+    "Sep  7 03:11:09 srv-web-01 sshd[4131]: Failed password for root from 203.0.113.42 port 51502 ssh2",
+    "Sep  7 03:11:02 srv-web-01 sshd[4127]: Failed password for invalid user admin from 203.0.113.42 port 51422 ssh2",
+    "Sep  7 06:02:17 srv-web-01 sshd[5021]: Accepted password for ahmed from 192.0.2.15 port 49802 ssh2",
+]
+
+tout = [ ]
+
+for ligne in lignes :
+
+    tout.extend(lignes.split())
+    
+
+list_ip  = []
+
+
+for i, mot in enumerate(tout) :
+    if mot == "from" :
+        list_ip.append(tout[i+1])
+
+print(list_ip )
